4b5d4e8ba45f21c55751accb74631b932e003043
k1r1ll <k1r1ll@k1r1ll.k1r1ll>
2025-12-01 17:48:11 +0300

    main_algorithm

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
-struct p_t{
+struct p_t
+{
   int x, y;
 };
 
-struct IDraw {
+struct IDraw 
+{
   virtual p_t begin() const = 0;
   virtual p_t next(p_t) const = 0;
 };
@@ -15,6 +17,7 @@ size_t count(IDraw& d)
   p_t p = d.begin();
 
 }
+
 namespace top{
   bool operator==(p_t a, p_t b)
   {
@@ -32,6 +35,13 @@ namespace top{
     p_t o;
     dot(int x, int y);
   };
+
+  void make_f(IDraw** b, size_t k);
+  void get_points(IDraw* b, p_t** ps, size_t &s);
+  frame_t build_frame(const p_t ps, size_t s);
+  char* build_carvas(frame_t f);
+  void build_canvas(char* cnv, frame_t fr, const p_t* ps, size_t k, char f);
+  void print_canvas(const char* cnv, frame_t fr);
 }
 
 top::dot::dot(int x, int y):
@@ -46,7 +56,32 @@ p_t top::dot::next(p_t) const {
   return begin();
 }
 
+struct frame_t{
+  p_t left_bot;
+  p_t right_top;
+};
+
 int main()
 {
   using namespace top;
+  IDraw* f[3] = {};
+  p_t* p = nullptr;
+  size_t s = 0;
+  int err = 0;
+  try 
+  {
+    make_f(f, 3);
+    for (size_t i = 0; i < 3; ++i)
+    {
+      get_points(f[i], &p, s);
+    }
+    frame_t fr = build_frame(*p, s);
+    cnv = build_canvas();
+    paint_canvas(cnv, fr, p, s, '#');
+    print_canvas(cnv, fr);
+  }
+  catch(...)
+  {
+    err = 1;
+  }
 }
