6e1c59618c39860a375fcbbbe7bedc3f9580cacc
kirill57701 <kirillpetrov56333@gmail.com>
2025-12-25 15:21:44 +0300

    Update cli.cpp

--- cli.cpp
+++ cli.cpp
@@ -1,18 +1,25 @@
+#include <cctype>
 #include <iostream>
 
 void hi() {
     std::cout << "<HI!>\n";
 }
 
+std::istream& getword(std::istream& is, char* word, size_t k, bool(*)(char));
+size_t match(const char * word, size_t bsize, const char * const * words, size_t k);
+
 int main() {
     const size_t cmds_count = 1;
     size_t i = 0;
     void(*cmds[cmds_count])() = {hi};
-    while (!(std::cin >> i).eof()) {
+    const char * const cmds_text[] = {"hi"};
+    constexpr size_t bsize = 255;
+    char word[bsize + 1] = {};
+    while (!(getword(std::cin, word, bsize, is_space)).eof()) {
         if (std::cin.fail()) {
             std::cerr << "<INVALID COMMAND>\n";
             return 1;
-        } else if (i < cmds_count) {
+        } else if (size_t i = match(word, cmds_text, cmds_count); i < cmds_count) {
             cmds[i]();
         } else {
             std::cerr << "<UNIKNOWN COMMAND>\n";
