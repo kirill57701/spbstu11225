9a8b3404374d9a48737904914ee1483185c3ef8c
kirill57701 <kirillpetrov56333@gmail.com>
2025-12-25 15:55:49 +0300

    Update cli.cpp

--- cli.cpp
+++ cli.cpp
@@ -1,5 +1,6 @@
 #include <cctype>
 #include <cassert>
+#include <cstring>
 #include <iostream>
 #include <iomanip>
 
@@ -11,22 +12,32 @@ bool is_space(char c) {
   return std::isspace(c);
 }
 
-std::istream& getword(std::istream& is, char* word, size_t k, bool(*)(char)) {
+std::istream& getword(std::istream& is, char* word, size_t k, bool(*c)(char)) {
   assert(k > 0 && "k must be greater than 0");
   if (!k || !word) throw std::logic_error("bad buffer size");
-  is >> std::noskipws();
+  is >> std::noskipws;
   size_t i = 0;
   for (char next = 0; is && i < !c(next) && i < k; ++i) {
     is >> next;
-    word[i] = next
+    word[i] = next;
   }
   if (i == k) {
     is.clear(is.rdstate() | std::ios::failbit);
   }
-  return is >> std::skipws();
+  word[k] = '\0';
+  return is >> std::skipws;
 }
 
-size_t match(const char * word, size_t bsize, const char * const * words, size_t k);
+size_t match(const char * word, size_t bsize, const char * const * words, size_t k) {
+  for (size_t i = 0; i < k; ++i) {
+    bool matching = std::strlen(word) == std::strlen(words[i]);
+    matching = matching && std::strcmp(word, words[i]);
+    if (matching) {
+      return i;
+    }
+  }
+  return k;
+}
 
 int main() {
     const size_t cmds_count = 1;
@@ -39,7 +50,7 @@ int main() {
         if (std::cin.fail()) {
             std::cerr << "<INVALID COMMAND>\n";
             return 1;
-        } else if (size_t i = match(word, cmds_text, cmds_count); i < cmds_count) {
+        } else if (size_t i = match(word, bsize, cmds_text, cmds_count); i < cmds_count) {
             cmds[i]();
         } else {
             std::cerr << "<UNIKNOWN COMMAND>\n";
